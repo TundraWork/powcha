102510f667141083783f538ce9aa695a19ef6f49
Tundra Work <tundrawork@gmail.com>
2024-07-30 00:25:44 +1000

    fix: trully use CORSAllowOrigins in config file

--- router.go
+++ router.go
@@ -5,7 +5,8 @@ import (
 
 	"github.com/cloudwego/hertz/pkg/app/server"
 	cors "github.com/tundrawork/hertz-cors"
-	handler "github.com/tundrawork/powcha/biz/handler"
+	"github.com/tundrawork/powcha/biz/handler"
+	"github.com/tundrawork/powcha/config"
 )
 
 // customizeRegister registers customize routers.
@@ -13,8 +14,7 @@ func customizedRegister(r *server.Hertz) {
 	r.GET("/ping", handler.Ping)
 
 	cors := cors.New(cors.Config{
-		//AllowOrigins:     config.Conf.CORSAllowOrigins,
-		AllowOrigins:     []string{"*"},
+		AllowOrigins:     config.Conf.CORSAllowOrigins,
 		AllowMethods:     []string{"OPTIONS", "GET", "POST"},
 		AllowHeaders:     []string{"*"},
 		ExposeHeaders:    []string{"*"},
